aac8afbdc2ecdeee3a7711406c1bbc6e88b7ee65
AbibullaevAR <a.alim2019@mail.ru>
2025-03-01 17:59:55 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-text-field
+        :rules="rules"
+        hide-details="auto"
+        label="Main input"
+      ></v-text-field>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  const rules = [
+    (value: string) => !!value,
+  ]
+</script>
